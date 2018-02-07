95550a3dd92142b6251a607ed6a5bcb2c935a276
tianle <1401734364@qq.com>
2018-02-07 14:51:35 +0800

    娴嬭瘯2

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
 
 class Home {
     public function index () {
-        echo "欢迎进入我的世界111";
+        echo "欢迎进入我的世界1113333";
     }
 
     public function pagesizes () {
-        echo "这里是我的列表页222";
+        echo "这里是我的列表页2224444";
     }
 }
 ?>
