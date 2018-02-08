6016372f9dd3be97ad5d2af8a12113958860171a
tianle <1401734364@qq.com>
2018-02-08 14:54:25 +0800

    推送

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
 
 class Home {
     public function index () {
-        echo "欢迎进入我的世界aaaa34";
+        echo "欢迎进入我的世界aaaa345";
     }
 
     public function pagesizes () {
-        echo "这里是我的列表页bbbb34";
+        echo "这里是我的列表页bbbb345";
     }
 }
 ?>
